20cef532b54e11029491ad110a1fbdcc5c645e5d
MzSuwawa <yudharamadhan838@gmail.com>
2025-10-23 13:36:04 +0900

    Menambahkan Spacer dan Card dengan lebar penuh serta padding

--- app/src/main/java/com/example/composelayout/Layout.kt
+++ app/src/main/java/com/example/composelayout/Layout.kt
@@ -1,6 +1,9 @@
 package com.example.composelayout
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.LineHeightStyle
@@ -16,5 +19,9 @@ fun ActivitasPertama(modifier: Modifier){
             fontWeight = FontWeight.Bold)
         Text(text = stringResource(id = R.string.univ),
             fontSize = 22.sp)
+        Spacer(modifier = Modifier.height(height = 25.dp))
+        Card (modifier = Modifier
+            .fillMaxWidth(fraction = 1f)
+            .padding(all = 12.dp))
     }
 }
