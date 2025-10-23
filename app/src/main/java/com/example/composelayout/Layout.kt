50703b576ef162532948b84843b9bd06c36b457c
MzSuwawa <yudharamadhan838@gmail.com>
2025-10-23 13:53:48 +0900

    Memperbaiki beberapa posisi kode dan menambahkan warna latar belakang DarkGray pada Card

--- app/src/main/java/com/example/composelayout/Layout.kt
+++ app/src/main/java/com/example/composelayout/Layout.kt
@@ -1,27 +1,35 @@
 package com.example.composelayout
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ActivitasPertama(modifier: Modifier){
     column(modifier = Modifier.padding(top=100.dp)
         .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = stringResource(id = R.string.prodi),
+        ) {
+        Text(stringResource(R.string.prodi),
             fontSize = 35.sp,
             fontWeight = FontWeight.Bold)
-        Text(text = stringResource(id = R.string.univ),
+        Text(stringResource(R.string.univ),
             fontSize = 22.sp)
-        Spacer(modifier = Modifier.height(height = 25.dp))
+        Spacer(modifier = Modifier.height(25.dp))
         Card (modifier = Modifier
-            .fillMaxWidth(fraction = 1f)
-            .padding(all = 12.dp))
+            .fillMaxWidth(1f)
+            .padding(12.dp))
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )) {
+    }
     }
 }
