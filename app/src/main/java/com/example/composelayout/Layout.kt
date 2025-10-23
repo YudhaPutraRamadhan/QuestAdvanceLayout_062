fd210ee7f54b935c31a5f233a5f520cdd47b5dc2
MzSuwawa <yudharamadhan838@gmail.com>
2025-10-23 14:28:00 +0900

    Menambahkan import untuk komponen Color dan Row

--- app/src/main/java/com/example/composelayout/Layout.kt
+++ app/src/main/java/com/example/composelayout/Layout.kt
@@ -2,6 +2,7 @@ package com.example.composelayout
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
