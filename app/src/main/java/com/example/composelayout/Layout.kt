d1dd8b726522d77192691d41535ee83381377a62
MzSuwawa <yudharamadhan838@gmail.com>
2025-10-23 13:59:07 +0900

    Menambahkan komponen gambar logo UMY di dalam Row layout dan Spacer di dalam Row untuk memberi jarak antar elemen

--- app/src/main/java/com/example/composelayout/Layout.kt
+++ app/src/main/java/com/example/composelayout/Layout.kt
@@ -3,10 +3,13 @@ package com.example.composelayout
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.LineHeightStyle
@@ -26,10 +29,19 @@ fun ActivitasPertama(modifier: Modifier){
         Spacer(modifier = Modifier.height(25.dp))
         Card (modifier = Modifier
             .fillMaxWidth(1f)
-            .padding(12.dp))
+            .padding(12.dp),
             colors = CardDefaults.cardColors(
                 containerColor = Color.DarkGray
             )) {
+            Row(){
+                val gambar = painterResource(R.drawable.logo_umy)
+                image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size(100.dp).padding(5.dp)
+                )
+                Spacer(modifier = Modifier.width(30.dp))
+            }
     }
     }
 }
