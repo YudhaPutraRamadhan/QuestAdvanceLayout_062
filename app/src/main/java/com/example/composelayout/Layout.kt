0c0920ba5bb1db490643838c9b5b796628483bae
MzSuwawa <yudharamadhan838@gmail.com>
2025-10-23 13:21:25 +0900

    Membuat project pertama dan menambahkan composable ActivitasPertama dengan tata letak Column di tengah

---
+++ app/src/main/java/com/example/composelayout/Layout.kt
@@ -0,0 +1,14 @@
+package com.example.composelayout
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.LineHeightStyle
+
+@Composable
+fun ActivitasPertama(modifier: Modifier){
+    column(modifier = Modifier.padding(top=100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
