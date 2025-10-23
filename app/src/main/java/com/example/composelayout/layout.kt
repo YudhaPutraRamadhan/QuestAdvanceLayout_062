ed82c3e58c23aa803e54163d1f003ad70a8e59b5
MzSuwawa <yudharamadhan838@gmail.com>
2025-10-23 14:30:47 +0900

    Mengatur ActivitasPertama sebagai konten utama di dalam Scaffold dengan menerapkan inner padding yang sesuai.

--- app/src/main/java/com/example/composelayout/layout.kt
+++ app/src/main/java/com/example/composelayout/layout.kt
@@ -1,6 +1,5 @@
 package com.example.composelayout
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,11 +17,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
