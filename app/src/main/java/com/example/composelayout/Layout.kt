108c3c6567a6ef26294fd18a5b6b36d52b1a1219
MzSuwawa <yudharamadhan838@gmail.com>
2025-10-23 14:28:40 +0900

    Menambahkan import untuk komponen Box

--- app/src/main/java/com/example/composelayout/Layout.kt
+++ app/src/main/java/com/example/composelayout/Layout.kt
@@ -1,6 +1,7 @@
 package com.example.composelayout
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
